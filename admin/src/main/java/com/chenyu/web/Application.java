42827a57258adf9ce550100b43eaf314525bb21a
chenyu <2856137514@qq.com>
2021-09-30 13:24:43 +0800

    项目初始构建

---
+++ admin/src/main/java/com/chenyu/web/Application.java
@@ -0,0 +1,18 @@
+package com.chenyu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: learnRuoYi
+ * @description: 主启动类
+ * @author: chen yu
+ * @create: 2021-09-30 11:41
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ framework/src/main/java/com/chenyu/framework/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.chenyu.framework.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+/**
+ * @program: learnRuoYi
+ * @description: SpringSecurity配置类
+ * @author: chen yu
+ * @create: 2021-09-30 13:08
+ */
+@EnableGlobalMethodSecurity
+public class SecurityConfig {
+    // 用户自定义验证逻辑
+
+    //认证失败处理类
+
+    //推出处理类
+
+
+}
